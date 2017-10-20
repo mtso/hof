d6bfe9d0f844868f1a5e4ba764461b8ed1349973
M <mtso@tuta.io>
2017-10-20 13:27:55 -0700

    Update and rename hof.py to python3.py

---
+++ python3.py
@@ -0,0 +1,16 @@
+"""
+Python 3.6.1
+"""
+
+def add(x):
+    "Return a function"
+    return lambda y: x + y
+
+def each(items, callback):
+    "Execute callback function parameter on each item in the items"
+    for item in items:
+        callback(item)
+
+"Example of calling the functions"
+add(1)(2)
+each([1, 2, 3], print)
