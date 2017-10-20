56106ec9000610981e0bd660d39f3d2d9de4bf1a
M <mtso@tuta.io>
2017-10-20 10:59:12 -0700

    Update hof.py

--- hof.py
+++ hof.py
@@ -1,11 +1,15 @@
-def add(x):
-    "Return a function"
-    return lambda y: x + y
+"""
+Python 3.6.1
+"""
 
-def each(list, callback):
-    "Execute callback function parameter on each item in the list"
-    for item in list:
-        callback(item)
+def add(x):
+  "Return a function"
+  return lambda y: x + y
+
+def each(items, callback):
+  "Execute callback function parameter on each item in the items"
+  for item in items:
+    callback(item)
 
 add(1)(2)
 each([1, 2, 3], print)
