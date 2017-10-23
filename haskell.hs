ffccb8cea7c25268f3307d7215103651243c6e1b
jndrk <jndrk.dev@gmail.com>
2017-10-23 19:20:12 +0200

    Add haskell main function

--- haskell.hs
+++ haskell.hs
@@ -1,13 +1,24 @@
 module Hof (add, each)
 where
 
--- returns a function adding `x` to its parameter
+import System.IO
+
+-- Returns a function adding `x` to its parameter
 -- ex: (add 5) 6    ==> 11
 add :: Num a => a -> a -> a
 add x = (+) x
 
 
--- applies `callback` function to `items` and returns a list with the results
+-- Applies `callback` function to `items` and returns a list with the results
 -- ex: each [1, 2, 3] (add 1)    ==> [2, 3, 4]
 each :: [a] -> (a -> b) -> [b]
 each items callback = map callback items
+
+
+-- Main function for example purposes
+main :: IO ()
+main = do
+  let x = (add 5) 6
+  putStrLn $ "Calculating:(add 5) 6 ==> " ++ show x
+  let xs = each [1, 2, 3] (add 1)
+  putStrLn $ "Calculating: each [1, 2, 3] (add 1) ==> " ++ show xs
