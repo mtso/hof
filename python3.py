634458fd344e6288c6b7d371523c334c06f16545
M <mtso@tuta.io>
2017-10-28 23:38:20 -0700

    Fix docstrings

--- python3.py
+++ python3.py
@@ -1,16 +1,14 @@
-"""
-Python 3.6.1
-"""
+# Python 3.6.1
 
 def add(x):
-    "Return a function"
+    """ Return a function """
     return lambda y: x + y
 
 def each(items, callback):
-    "Execute callback function parameter on each item in the items"
+    """ Execute callback function parameter on each item in the items """
     for item in items:
         callback(item)
 
-"Example of calling the functions"
+# Example of calling the functions
 add(1)(2)
 each([1, 2, 3], print)
