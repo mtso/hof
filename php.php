e0c01d4513b97e672a7fa11bf5f6c1bb456faa89
Mark Gerarts <mark.gerarts@gmail.com>
2018-10-11 16:33:47 +0200

    Create PHP implementation

---
+++ php.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace HOF;
+
+// Returns a function.
+function add(int $x): callable {
+    return function (int $y) use ($x): int {
+        return $x + $y;
+    };
+}
+
+// Accepts a function.
+function each(iterable $list, callable $callback): void {
+  foreach ($list as $element) {
+    $callback($element);
+  }
+}
+
+// Examples.
+add(1)(2);
+each([1, 2, 3], 'var_dump');
