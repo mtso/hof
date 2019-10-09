ec66b0ce105b568679c7330879f9359c3df962fe
Ben <benbals+accounts@posteo.de>
2019-10-09 08:45:36 +0200

    Create Elm.elm
    This improves on #12 to address the concerns about imports you mentioned and use the new Browser API.

---
+++ Elm.elm
@@ -0,0 +1,38 @@
+module HOF exposing (main)
+
+import Html exposing (Html, text, div)
+import String exposing (fromInt)
+import List
+import Browser
+
+add : Int -> Int -> Int
+add a =
+    (+) a
+
+each : (a -> b) -> List a -> List b
+each f list =
+    case list of
+        [] ->
+            []
+        x :: xs ->
+            f x :: each f xs
+
+printHtmlString : Int -> Html ()
+printHtmlString =
+    text << fromInt
+
+view : () -> Html ()
+view _ =
+    div []
+        ([ printHtmlString <| add 1 2 ] ++ each printHtmlString [ 1, 2, 3 ])
+
+update : () -> () -> ()
+update _ _ = ()
+
+main : Program () () ()
+main =
+    Browser.sandbox
+        { init = ()
+        , view = view
+        , update = update
+        }
