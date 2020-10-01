81558542a0798b0f6507bc07c8da50cb97ac2aee
Matei Adriel <rafaeladriel11@gmail.com>
2020-10-01 16:40:54 +0300

    Updated purescript example
    I made it:
    - use uncons instead of head + tail inside each
    - use logShow instead of log $ show
    - use the synax for pattern matching on multiple values at the same time instead of tuples

--- purescript.purs
+++ purescript.purs
@@ -2,20 +2,18 @@ module Main where
 
 import Prelude
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console (logShow)
 import Data.Array
 import Data.Maybe
-import Data.Tuple
 
 main :: Effect Unit
 main = do
   let sum = (add 5) 6
-  log $ show sum
-  let out = each (add 7) ([1,2,3,4])
-  log $ show out
-  let out2 = each2 (add 7) ([1,2,3,4])
-  log $ show out2
-
+  logShow sum
+  let out = each (add 7) ([ 1, 2, 3, 4 ])
+  logShow out
+  let out2 = each2 (add 7) ([ 1, 2, 3, 4 ])
+  logShow out2
 
 -- much like Haskell, this Returns a function adding `x` to its parameter. 
 add :: Int -> Int -> Int
@@ -23,17 +21,14 @@ add = (+)
 
 -- Applies function `fn` to each element in the list 
 -- effectively a map function
-each :: forall a b.  (a -> b) -> Array a -> Array b 
-each fn arr = case head arr of 
-  Just x -> case tail arr of 
-    Just xs -> (fn x) : (each fn xs)
-    Nothing -> [ (fn x) ]
+each :: forall a b. (a -> b) -> Array a -> Array b
+each fn arr = case uncons arr of
+  Just res -> (fn res.head) : (each fn res.tail)
   Nothing -> []
 
-
 -- Slightly different way of writing it
-each2 :: forall a b. (a -> b) -> Array a -> Array b 
-each2 fn arr = case Tuple (head arr) (tail arr) of 
-  Tuple (Just x) (Just xs) -> (fn x) : (each2 fn xs)
-  Tuple (Just x) (Nothing) -> [(fn x)]
-  Tuple ( _ ) ( _ ) -> []
+each2 :: forall a b. (a -> b) -> Array a -> Array b
+each2 fn arr = case (head arr), (tail arr) of
+  Just x, Just xs -> (fn x) : (each2 fn xs)
+  Just x, Nothing -> [ (fn x) ]
+  _, _ -> []
